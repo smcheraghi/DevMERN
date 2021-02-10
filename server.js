e5f2d7448975aca54f182481ae9e53e3bacda8e7
seyed mohammad cheraghi <35502397+smcheraghi@users.noreply.github.com>
2021-02-10 02:03:29 -0500

    Make database connection

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Connect DB
+connectDB();
+
 app.get('/', (req, res) => {
-    res.send('hi')
-})
+  res.send('hi');
+});
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
